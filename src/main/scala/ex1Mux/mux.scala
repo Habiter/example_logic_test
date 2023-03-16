174dbe2d9c822e8d51b0a483fd0d19485874625d
Yuanzhi <ha_biter@163.com>
2023-03-16 16:44:39 +0800

    3.17 git again

--- src/main/scala/ex1Mux/mux.scala
+++ src/main/scala/ex1Mux/mux.scala
@@ -1,5 +1,11 @@
 package ex1Mux
-
-class mux {
-
+import chisel3._
+class mux extends Module {
+    val io = IO{ new Bundle() {
+        val a = Input(Bool())
+        val b = Input(Bool())
+        val s = Input(Bool())
+        val y = Output(Bool())
+    }}
+    io.y := (io.a && (!io.s)) || (io.b && (io.s))
 }
--- src/main/scala/main_verigen.scala
+++ src/main/scala/main_verigen.scala
@@ -1,3 +1,8 @@
-class main_verigen {
-
+import chisel3._
+import ex1Mux._
+object main extends App {
+    println(s"hello scala")
+    println(s"hello chisel")
+    val str_mux = getVerilogString(new mux())
+    println(str_mux)
 }
